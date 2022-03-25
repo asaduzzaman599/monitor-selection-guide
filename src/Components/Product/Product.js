e10458256d2dec06a702a5de46cb6f6b8aa05b97
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-03-25 23:21:52 +0600

    fontawesome add

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import './Product.css'
+import './Product.css'  
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
-const Product = () => {
+const Product = ({product}) => {
+    console.log(product)
+    const {name,image,price} = product;
     return (
-        <div>
-            <h3>product</h3>
+        <div className='product'>
+            <img src={image} alt="" />
+            <h3>{name}</h3>
+            <p><b>Price: </b>{price}BDT</p>
+            <button>ADD TO CART <FontAwesomeIcon icon={faShoppingCart} /></button>
         </div>
     );
 };
