10b81698fd5aa24d32b603f9dd8285acff071d68
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-03-25 22:33:43 +0600

    header added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import './Header.css'
 const Header = () => {
     return (
-        <div>
-            <h1></h1>
+        <div className='header'>
+            <nav className='nav'>
+                <h2>
+                    Monitor Selector
+                </h2>
+            </nav>
         </div>
     );
 };
