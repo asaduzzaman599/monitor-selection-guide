7f74485a5c0d79c2d2964c86a09775779669ddec
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-03-25 23:31:51 +0600

    cart item event handler added

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -3,15 +3,14 @@ import './Product.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
-const Product = ({product}) => {
-    console.log(product)
+const Product = ({product,addToCart}) => {
     const {name,image,price} = product;
     return (
         <div className='product'>
             <img src={image} alt="" />
             <h3>{name}</h3>
             <p><b>Price: </b>{price}BDT</p>
-            <button>ADD TO CART <FontAwesomeIcon icon={faShoppingCart} /></button>
+            <button onClick={()=>addToCart(product)}>ADD TO CART <FontAwesomeIcon icon={faShoppingCart} /></button>
         </div>
     );
 };
--- src/Components/Shop/Shop.js
+++ src/Components/Shop/Shop.js
@@ -3,6 +3,9 @@ import Product from '../Product/Product';
 import './Shop.css'
 const Shop = () => {
     const [products,setProucts] = useState([]);
+    const [cartItems,setCartItems] = useState([]);
+
+
     useEffect(
         ()=>{
             fetch('fakedata/data.json')
@@ -11,11 +14,27 @@ const Shop = () => {
         }
         ,[]
     )
+
+    const addToCart = (product) =>{
+        const exist = cartItems.find(item=> item.id === product.id);
+
+        if(exist){
+            console.log("not",product)
+        }else{
+            if(cartItems.length < 5){
+                const newCart = [...cartItems,product];
+                setCartItems(newCart)
+            console.log(cartItems,product)
+            }else{
+                alert('More Then 4 not allowed')
+            }
+        }
+    }
     return (
         <div className='shop-container'>
             <div className="product-container">
                 {
-                    products.map(product=> <Product key={product.id} product={product}></Product>)
+                    products.map(product=> <Product key={product.id} addToCart={addToCart} product={product}></Product>)
                 }
             </div>
             <div className="cart-container">
